abb9919b3f787c837b8d7c03753806ef56b533df
Cyrus <cyrussea@gmail.com>
2013-05-16 16:39:04 -0700

    sigSnvs2pgSnp.pl bug corrected and updated

--- sigSnvs2pgSnp.pl
+++ sigSnvs2pgSnp.pl
@@ -4,6 +4,13 @@ use warnings;
 
 require "fileParser.pl"; #sub's for input annotation files
 require "snpParser.pl"; #sub's for snps
+use MyConstants qw( $CHRNUM );
+
+# set autoflush for error and output
+select(STDERR);
+$| = 1;
+select(STDOUT);
+$| = 1;
 
 if(@ARGV<4){
   print <<EOT;
@@ -22,7 +29,8 @@ pgSNP information can be found at:
 http://main.genome-browser.bx.psu.edu/FAQ/FAQformat.html#format10
 
 ARGUMENTS:
-asarp_file	the ASARP result file, serving as the prefix for accessing
+asarp_file	the ASARP result path/file name, serving as the prefix for 
+		accessing
 		"asarp_file.gene.prediction" and 
 		"asarp_file.controlSNV.prediction"
 		of the ASARP pipeline
@@ -31,11 +39,24 @@ outfolder	output folder for the pgSnp and .aux files
 annotations	transcript annotation file, which is also required for
 		the ASARP pipeline (e.g. hg19.merged...txt)
 
+OPTIONAL:
+top_k		the top target-ctrl pairs (w.r.t p-values) to look at in 
+		visualization, if not input (all <= 0), all pairs will 
+		be output
+description	a description string (quoted when space is used) to identify
+		the current sample/dataset, e.g. "gm12878 encode cytosol"
 EOT
   exit;
 }
 
-my ($input, $db, $outFolder, $xiaoF) = @ARGV;
+my ($input, $db, $outFolder, $xiaoF, $topK, $idString) = @ARGV;
+
+if(!defined($topK) || $topK <=0){
+   $topK = 0;
+}
+if(!defined($idString)){
+  $idString = 'default';
+}
 
 open(FP, "$input.controlSNV.prediction") or die "ERROR: cannot open $input.controlSNV.prediction to read\n";
 my @pred = <FP>;
@@ -78,11 +99,14 @@ for(my $i = 1; $i < @predGenes; $i++){
     }
   }
 }
+#print "pgSnp Track done\n";
 # finished (just want the SNP ID, allelels, etc.)
 
 my %pgAux = ();
 my %chrStart = ();
 my %chrEnd = ();
+# all p-values for shortlisting
+my @allPs = ();
 
 # read the transcript annotation file
 my $transRef = readTranscriptFile($xiaoF);
@@ -113,21 +137,24 @@ for(my $i = 1; $i < @pred; $i++){
       my $geneS = $geneStarts[$chrId]->{$gene};
       my $geneE = $geneEnds[$chrId]->{$gene};
      
-      print "searching for $gene start:end $geneS:$geneE\n";
+      #print "$gene start:end $geneS:$geneE\n";
       my ($loc, $unMatchFlag) = binarySearch($chrTransIdxRef, $geneS, 0, @chrTransIdx-1, 'left');  
       # it must match otherwise the TransIdx is wrong
       if($unMatchFlag){
         die "ERROR: $gene start: $geneS must match some transcripts in $chr\n";
       }
-      print "$gene starts in idx[$loc]\n"; #: $chrTransIdx[$loc] of \n"; #@chrTransIdx\n";
-      
+      #print "$gene starts in idx[$loc]\n"; print "#: $chrTransIdx[$loc]\n"; # of @chrTransIdx\n";
+      #print "$geneS to $geneE\n";
       #trans structure from fileParser.pl
       #  $trans[$chrId]{$txStart}.=$txEnd.";".$cdsStart.";".$cdsEnd.";".$exonStarts.";".$exonEnds.";".$ID.";".uc($geneName).";".$isCoding.";".$strand."\t";
       my $ti = $loc;
       %exs =(); #re-initialize
       @sortedExs = ();
       while($ti < @chrTransIdx && $chrTransIdx[$ti] <= $geneE){
-        if($chrTrans{$chrTransIdx[$ti]} =~ /;$gene;/){
+	my $geneStub = ";$gene;";
+        #if($chrTrans{$chrTransIdx[$ti]} =~ /$geneStub/){ # gene names have '+', need to escape!!!
+        if(index($chrTrans{$chrTransIdx[$ti]}, $geneStub)!=-1){
+	  
           my @allEnds = split('\t', $chrTrans{$chrTransIdx[$ti]});	
 	  for(@allEnds){
 	    my ($txEnd, $cdsS, $cdsE, $exonSs, $exonEs, $txId, $txGene) = split(';', $_);
@@ -154,20 +181,24 @@ for(my $i = 1; $i < @pred; $i++){
       #get target-control pairs
       my ($catInfo, $pVal, $trgtPos, $ctrlPos) = split(';', $pred[$i]);
       if(defined($bestP{$trgtPos}) && $bestP{$trgtPos} <= $pVal){ #no need to look at this case
+	$i++;
 	next; # no need to see this any more
       }
       #update info
       $bestP{$trgtPos} = $pVal;
-
+      #print "$gene;$trgtPos\n";
       #use @sortedEx and %exs to get the left and right flanking regions
+      if(@sortedExs == 0){
+        die "ERROR: cannot find exon end sets for $gene\n";
+      }
       my ($rangeL, $rangeR) = getBoundary($trgtPos, \%exs, \@sortedExs, 1);
-      my ($ctrlRangeL, $ctrlRangeR) = getBoundary($ctrlPos, \%exs, \@sortedExs, 0); # no need to get flanking regions as NEV is not required for ctrlPos
-      if($ctrlRangeL < $rangeL){ $rangeL = $ctrlRangeL; }
-      if($ctrlRangeR > $rangeR){ $rangeR = $ctrlRangeR; }
+      #my ($ctrlRangeL, $ctrlRangeR) = getBoundary($ctrlPos, \%exs, \@sortedExs, 0); # no need to get flanking regions as NEV is not required for ctrlPos
+      #if($ctrlRangeL < $rangeL){ $rangeL = $ctrlRangeL; }
+      #if($ctrlRangeR > $rangeR){ $rangeR = $ctrlRangeR; }
 
       # now we need %geneTrgts
       my $snpInfo = $geneTrgts{"$gene;$trgtPos"};
-      $coms{"$gene;$trgtPos"} = join("\t", $chr, $rangeL, $rangeR, "$gene $snpInfo (vs $ctrlPos p: $pVal) type: $catInfo");
+      $coms{"$gene;$trgtPos"} = join("\t", $chr, $rangeL, $rangeR, "$gene $snpInfo (vs:$ctrlPos p:$pVal) type: $catInfo");
       
       if(!defined($chrStart{$chr})){  $chrStart{$chr} = $trgtPos;  }
       elsif($chrStart{$chr} > $trgtPos){	$chrStart{$chr} = $trgtPos;	}
@@ -180,21 +211,62 @@ for(my $i = 1; $i < @pred; $i++){
     for(keys %coms){
       $pgAux{$chr} .= $coms{$_}."\n";
     }
+    for(keys %bestP){
+      push (@allPs, $bestP{$_});
+    }
   }
 }
+
+my $pSize = @allPs;
+print "There are in total there are $pSize target ASARP SNVs\n";
+if($topK && $topK < $pSize){
+  my $cnt = 0;
+  print "Shortlist only the top pairs according to the p-value threshold cut at the top $topK p-values\n";
+  @allPs = sort{$a <=> $b}@allPs;  # sort ascending
+  my $cutoff = $allPs[$topK-1];
+
+  for(my $j=1; $j<=$CHRNUM; $j++){
+    my $chr = formatChr($j);
+    if(defined($pgAux{$chr})){
+      my @allCases = split(/\n/, $pgAux{$chr});
+      $pgAux{$chr} = '';
+      for(@allCases){
+        if($_ =~ /p:(.+)\) type:/){
+	  my $curP = $1;
+	  if($curP <= $cutoff){
+	    $pgAux{$chr} .= $_."\n";
+	    $cnt++;
+	  }#else{  print "Removed $_\n";  }
+	}else{
+	  die "ERROR: cannot find match of the p-value in $_\n";
+	}
+      }
+    }
+  }
+  print "Finally $cnt ASARP target SNVs are shortlisted according to top $topK p-value cutoff: $cutoff\n";
+}
+# done
+
+
+
 print "\nOutput pgSnp and aux to $outFolder\n";
 for(keys %pgSnp){
   my $chr = $_;
-  print "$chr\n";
+  #print "$chr\n";
   open(OP, ">", "$outFolder/$chr.pgSnp") or die "ERROR: cannot open $outFolder/$chr.pgSnp for writing\n";
-  print OP "track type=pgSnp visibility=3 db=$db name=\"SNV_$chr\" description=\"ASARP SNVs in $chr\"\n";
+  print OP "track type=pgSnp visibility=3 db=$db name=\"SNV_$chr\" description=\"$idString: ASARP SNVs in $chr\"\n";
   print OP "browser position $chr:$chrStart{$chr}-$chrEnd{$chr}\n";
   print OP $pgSnp{$chr};
   close(OP);
-  
-  open(AP, ">", "$outFolder/$chr.pgSnp.aux") or die "ERROR: cannot open $outFolder/$chr.pgSnp.aux for writing\n";
-  print AP $pgAux{$chr};
-  close(AP);
+} 
+
+for(my $j=1; $j<=$CHRNUM; $j++){
+  my $chr = formatChr($j);
+  if(defined($pgAux{$chr})){
+    open(AP, ">", "$outFolder/$chr.pgSnp.aux") or die "ERROR: cannot open $outFolder/$chr.pgSnp.aux for writing\n";
+    print AP $pgAux{$chr};
+    close(AP);
+  }
 }
 
 sub getBoundary{
@@ -207,6 +279,9 @@ sub getBoundary{
    my %exs = %$exsHsRef;
 
       my ($exIdx, $unMatch) = binarySearch(\@sortedExs, $pos, 0, @sortedExs-1, 'left'); 
+      if(!defined($exs{$sortedExs[-1]})){
+        die "Problem with @sortedExs, \n($exIdx, $unMatch) stop here\n";
+      }
       my ($rangeL, $rangeR) = ($sortedExs[0], $exs{$sortedExs[-1]}); #initialize $rangeL and $rangeR to the possible min and max (in case no matches)
       my $lIdx = 0;
       if($exIdx > 0){  $lIdx = $exIdx - 1; #the previous exon start
@@ -228,5 +303,6 @@ sub getBoundary{
       if(!$unMatch){
         return ($sortedExs[$exIdx], $exs{$sortedExs[$exIdx]}); # it's directly on an exon
       }
+      print "return ($sortedExs[$lIdx], $exs{$sortedExs[$lIdx]})\n";
       return ($sortedExs[$lIdx], $exs{$sortedExs[$lIdx]});
 }      
