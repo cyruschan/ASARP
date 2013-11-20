f13a2d24157e03125782b15edf599e053c19cba6
cyruschan <cyrussea@gmail.com>
2013-11-20 14:55:54 -0800

    Add unzip data/hg19.merged.to.ensg.all.tx.03.18.2011.txt.gz instructions

---
+++ demo/extract.hg19.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+hg19=../data/hg19.merged.to.ensg.all.tx.03.18.2011.txt
+
+line=">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo ">> Unzip $hg19 if necessary"
+echo ""
+
+echo "Checking existance of $hg19 ..."
+if [ ! -e $hg19 ]; then
+  echo "Unzipping $hg19.gz to $hg ..."
+  gunzip -c $hg19.gz >$hg19
+  if [ -e $hg19 ]; then
+    echo "[OK]"
+  else
+    echo "[FAILED]"
+    exit;
+  fi
+else 
+  echo "[OK]"
+fi
+
+echo "See if the file size is reasonable"
+ls -hs $hg19
